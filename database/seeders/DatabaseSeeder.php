383811c99ee51ede66c17a2301c8987798942f1f
VanLuong <van.luong.94114@gmail.com>
2021-12-02 15:16:43 +0700

    [add] laravel authentication

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +14,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::create([
+            'name'     => 'Super Admin',
+            'email'    => 'superadmin@aptech.com',
+            'password' => bcrypt('123456'),
+            'role'     => 'admin',
+        ]);
     }
 }
