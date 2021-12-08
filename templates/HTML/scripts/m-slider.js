09984d9242d266d3dd01f4b988b69ce6d22ff8d2
Van Luong <van.luong.94114@gmail.com>
2021-12-08 13:54:35 +0700

    [update] glider arrow

--- templates/HTML/scripts/m-slider.js
+++ templates/HTML/scripts/m-slider.js
@@ -1,6 +1,12 @@
-document.querySelectorAll('.glider').forEach( (e) => {
-	// new Glider(e, );
-	if (e.dataset.glider){
-		new Glider(e, JSON.parse(e.dataset.glider));
+document.querySelectorAll('.glider-contain').forEach( (e) => {
+	let glider = e.querySelector('.glider');
+	if (glider && glider.dataset.glider){
+		let opts = JSON.parse(glider.dataset.glider);
+
+		if (opts.arrows) {
+			opts.arrows.prev = e.querySelector(opts.arrows.prev);
+			opts.arrows.next = e.querySelector(opts.arrows.next);
+		}
+		new Glider(glider, opts);
 	}
 } );
