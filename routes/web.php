5d90a34d82cb134b578e5660acc73f536ec32f44
Van Luong <van.luong.94114@gmail.com>
2021-12-22 16:19:11 +0700

    [update] admin role authentication

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
 Route::get('/dashboard', function () {
     return view('admin.dashboard');
-})->middleware(['auth'])->name('dashboard');
+})->middleware(['auth.admin'])->name('dashboard');
 
 require __DIR__.'/auth.php';
