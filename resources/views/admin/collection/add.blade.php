0ab3229dae1f1ea344ff3bd8b121f84cb0825c92
Van Luong <van.luong.94114@gmail.com>
2021-12-30 17:11:53 +0700

    [update] add button link to listing page for collection add and edit page

--- resources/views/admin/collection/add.blade.php
+++ resources/views/admin/collection/add.blade.php
@@ -2,7 +2,14 @@
 
 @section('pageHeading')
     <!-- Page Heading -->
-    <h1 class="h3 mb-4 text-gray-800">Add New {{ App\Models\MangaCollection::getTypeLabel( $type ) }}</h1>
+
+    <!-- Page Heading -->
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 text-gray-800">Add New {{ App\Models\MangaCollection::getTypeLabel( $type ) }}</h1>
+        <a href="{{ route('admin.collection.all', $type) }}" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm">
+            <i class="fas fa-stream fa-sm text-white-50"></i> All {{ App\Models\MangaCollection::getTypeLabel( $type, true ) }}
+        </a>
+    </div>
 @endsection
 
 <x-dashboard-layout>
--- resources/views/admin/collection/edit.blade.php
+++ resources/views/admin/collection/edit.blade.php
@@ -2,7 +2,12 @@
 
 @section('pageHeading')
     <!-- Page Heading -->
-    <h1 class="h3 mb-4 text-gray-800">Edit {{ $collection->getTypeLabelSingular() }}</h1>
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 text-gray-800">Edit {{ $collection->getTypeLabelSingular() }}</h1>
+        <a href="{{ route('admin.collection.all', $type) }}" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm">
+            <i class="fas fa-stream fa-sm text-white-50"></i> All {{ $collection->getTypeLabelPlural() }}
+        </a>
+    </div>
 @endsection
 
 <x-dashboard-layout>
